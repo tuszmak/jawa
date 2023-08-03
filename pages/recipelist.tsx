385bc30ece3f75bfa2dac9374a232dfb4aa40643
tuszmak <daniel.pinter02@edu.bme.hu>
2023-08-03 11:51:36 +0200

    feat: implement a searchbar for recipes

--- pages/recipelist.tsx
+++ pages/recipelist.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { Props, Recipe } from "@/types/types";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 // async function getData() {
 //     let response = await fetch("http://localhost:3000/api/getRecipes")
@@ -9,13 +10,33 @@ import Link from "next/link";
 //     console.log(response);
 
 // }
+interface IRecipeListProps {
+  data: Recipe[]
+}
 
+export default function recipeList({ data }: IRecipeListProps) {
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [filteredData, setFilteredData] = useState<Recipe[]>(data)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
 
-export default function recipeList({ data }: Props) {
+  };
+  useEffect(()=>{
+    const filtered = data.filter((recipe)=>recipe.name.toLowerCase().includes(searchValue.toLowerCase()))
+    setFilteredData(filtered);
+  }, [searchValue])
   return (
     <div className="overflow-x-auto">
+      <div className="flex justify-end">
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+          value={searchValue}
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
       <table className="table">
-        {/* head */}
         <thead>
           <tr>
             <th></th>
@@ -25,14 +46,17 @@ export default function recipeList({ data }: Props) {
           </tr>
         </thead>
         <tbody>
-          {data.map((e : Recipe)=>(
-            <tr>
-            <th>{e?.id}</th>
-            <td>{e?.name}</td>
-            <td></td>
-            <td><button className="btn btn-outline"><Link href={`recipe/${e.id}`}>Details</Link></button></td>
-          </tr>
-          
+          {filteredData.map((e : Recipe, i: number)=>(
+            <tr key={i}>
+              <th>{e?.id}</th>
+              <td>{e?.name}</td>
+              <td></td>
+              <td>
+                <button className="btn btn-outline">
+                  <Link href={`recipe/${e.id}`}>Details</Link>
+                </button>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
