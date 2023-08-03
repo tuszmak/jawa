c0e268ef75d69ffca02e49d5e9b7acbe1a454e22
tuszmak <daniel.pinter02@edu.bme.hu>
2023-08-03 10:55:25 +0200

    feat: add a back button

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { Ingredient, Props } from "@/types/types";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 let id : number;
@@ -12,6 +13,7 @@ export default function recipe({data, ingredients}: Props) {
     }
     return(
         <div>
+            <button className="btn btn-primary"><Link href='/recipelist'>Back to list</Link></button>
             <h1>{data.name}</h1>
             <Image src={`/recipe${id}.jpg`} width={450} height={450} alt="Image here" />
             {ingredients.map((e: Ingredient) => <p>{e.name}</p>)}
