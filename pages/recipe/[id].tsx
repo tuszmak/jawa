97fbac7e88b2642f75d930e51993706b528840f2
tuszmak <daniel.pinter02@edu.bme.hu>
2023-08-03 10:20:35 +0200

    feat: show a recipe's ingredients

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -1,11 +1,11 @@
 import { prisma } from "@/lib/prisma";
-import { Props } from "@/types/types";
+import { Ingredient, Props } from "@/types/types";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
 let id : number;
-export default function recipe({data}: Props) {
+export default function recipe({data, ingredients}: Props) {
     const router = useRouter()
     if(router.query.id){
         id = parseInt(router.query.id.toString());
@@ -14,6 +14,7 @@ export default function recipe({data}: Props) {
         <div>
             <h1>{data.name}</h1>
             <Image src={`/recipe${id}.jpg`} width={450} height={450} alt="Image here" />
+            {ingredients.map((e: Ingredient) => <p>{e.name}</p>)}
             <p>{data.instructions}</p>
         </div>
     )
@@ -26,9 +27,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
             }
         }
     )
+    const ingredients = await prisma.ingredient.findMany(
+    {
+        where:{
+            id : {in: data?.ingredient_id_list}
+        }
+    }
+    )
     return {
       props: {
         data,
+        ingredients
       },
     };
   };
--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,8 @@ import { getServerSideProps } from "@/pages/recipelist";
 import { InferGetServerSidePropsType } from "next";
 
 export type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export type Ingredient = {
+    id: number,
+    name: string;
+}
