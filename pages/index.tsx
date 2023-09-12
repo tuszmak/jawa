0981a8a1b8ef66f437cf4f2900508b1a6cb35194
tuszmak <daniel.pinter02@edu.bme.hu>
2023-09-12 10:41:22 +0200

    feat: make the basic design of the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,40 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 import Link from "next/link";
+import HomeImage from "../images/photo-1481931098730-318b6f776db0.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen items-center justify-between ${inter.className}`}
     >
-      <div>
-        <p>Welcome to the homepage!</p>
-        <p>Please press the button that suits your needs!</p>
-        <button className="btn btn-outline btn-secondary m-4">
-          <Link href="/recipelist">List recipes</Link>
-        </button>
-        <button className="btn btn-outline">What should I cook?</button>
+      <div className="flex flex-col items-center h-screen lg:w-1/2">
+        <div className="h-1/3"></div>
+        <div className="flex flex-col gap-7 items-center">
+          <p className="text-2xl text-red-500">
+            Ran out of ideas? You don't know what to cook?
+          </p>
+          <p className="text-2xl">You came to the right place!</p>
+        </div>
+        <div className="m-10 flex flex-col gap-12 lg:flex-row ">
+          <button className="btn btn-outline btn-secondary ">
+            <Link href="/recipelist">List recipes</Link>
+          </button>
+          <button className="btn btn-outline" disabled>
+            What should I cook?
+          </button>
+        </div>
+      </div>
+      <div className="lg:w-1/2 h-screen flex content-end ">
+        <Image
+          src={HomeImage}
+          alt="Food"
+          width={0}
+          height={0}
+          className="hidden lg:flex"
+        />
       </div>
     </main>
   );
